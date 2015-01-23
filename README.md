7ffb3b83cf8d5206faa8fa655cf3bf7461380baa
Tom Chomiak <tom.chomiak@gmail.com>
2015-01-23 16:05:26 -0600

    Initial panoskin commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Panoskin.js, a client library used to load the Panoskin viewer
+Panoskin is the toolbelt to your Google Business tours. It adds a skinable, customizable, interactive UI to your existing Google Business tours. Panoskin also supports interactive image and video galleries, works with Google Analytics, and can be used to load a custom virutal tour from LCP360. Panoskin works on mobile, does not require you to load any other external libraries to work, and can be easily added within any webpage.
+
+## How it works
+Load Panoskin library by either referencing the latest minified version served from our CDN or through Bower. 
+
+```html
+<script src="//lcp360.cachefly.net/panoskin.min.js"></script>
+```
+
+`Bower install panoskin`
+
+Call the createViewer method after loading the Panoskin library, passing in an id of the container you want to load the tour into and the tour id that you want to load. Note, your domain(s) will need to be whitelisted for the tours you are attempting to load. 
+
+```html
+<div id="pano"></div>
+
+<script src="//lcp360.cachefly.net/panoskin.min.js"></script>
+<script>
+  PANOSKIN.createViewer({
+    id: 'pano',
+    tour: '/cambria/'
+  });
+</script>
+```
+
+Some optional CSS to make your container fluid
+
+```css
+  #pano {
+    width: 100%;
+    height: 70%;
+    max-height: 500px;
+    max-width: 700px;
+  }
+```
---
+++ panoskin.js
@@ -0,0 +1,96 @@
+var PANOSKIN = {
+
+    createViewer: function(obj) {
+
+        var id = obj.id,
+            tour = obj.tour,
+            url = (obj.dev) ? document.domain + ":" + location.port : 'panoskin.lcp360.com';
+
+        if (!obj.id || !obj.tour) return console.log('Please specifiy the id and tour link');
+
+        var iframe = document.createElement('iframe'), frameSrc = "//" + url + '/_.html?id=' + id + '&tour=' + tour;
+        iframe.src = (obj.legacy) ? frameSrc + "&legacy=true" : frameSrc;
+        iframe.style.width = "inherit";
+        iframe.style.height = "inherit";
+        iframe.style.border = "none";
+        iframe.frameBorder = "none";
+        iframe.setAttribute('allowtransparency', 'true');
+        iframe.setAttribute('scrolling', 'no');
+        document.getElementById(id).appendChild(iframe);
+    },
+    event: function(el, event, fnc) {
+
+        if (el.attachEvent) el.attachEvent("on" + event, fnc);
+        else if (el.addEventListener) el.addEventListener(event, fnc, false);
+
+    },
+    GA: function(param) {
+
+        // Fire Analytics
+        var _gaq = _gaq || [];
+
+        if (param.trackPageView) {
+
+            console.log('Track Page View: ' + param.trackPageView);
+            _gaq.push(['_trackPageview', param.trackPageView]);
+        }
+
+        // Debug Mode
+        if (param.panoid && param.debug) console.log("Moving to pano: " + param.panoid + "\n\n" + "Tracking as: " + param.trackPageView);
+        else if (param.debug) console.log("Tracking as: " + param.trackPageView);
+        if (param.redirect) document.location.href = param.redirect;
+
+    },
+    fullScreen: function(param) {
+
+        var id = param.id,
+            prefix = ["webkit", "moz", "o", "ms", ""],
+            fullScreenFnc = function(e, r) {
+                for (var i, f, t = 0; t < prefix.length && !e[i];) {
+                    if (i = r, "" == prefix[t] && (i = i.substr(0, 1).toLowerCase() + i.substr(1)), i = prefix[t] + i, f = typeof e[i], "undefined" != f) return prefix = [prefix[t]], "function" == f ? e[i]() : e[i];
+                    t++
+                }
+            };
+
+        if (!id) return;
+
+        var el = document.getElementById(id);
+
+        if (el.getAttribute('data-attr-fullscreen') || fullScreenFnc(document, "FullScreen") || fullScreenFnc(document, "IsFullScreen")) {
+
+            el.setAttribute('style', el.getAttribute('data-attr-style-bk'));
+            el.removeAttribute('data-attr-style-bk');
+            el.removeAttribute('data-attr-fullscreen');
+            fullScreenFnc(document, "CancelFullScreen");
+
+        } else {
+
+            fullScreenFnc(el, "RequestFullScreen");
+            el.setAttribute('data-attr-style-bk', el.getAttribute('style'));
+            el.setAttribute('style', 'position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 1000000; max-width: 100%; max-height: 100%;');
+            el.setAttribute('data-attr-fullscreen', 'true');
+        }
+
+    },
+    redirect: function(param) {
+        if (param.url) document.location = param.url;
+    }
+}
+
+
+// Cross Frame Event
+PANOSKIN.event(window, 'message', function (e) {
+
+    var domain = e.origin.split('viewer.')[1] || e.origin,
+        data = JSON.parse(e.data);
+        domain = domain.split('.com')[0] || domain;
+
+	/*
+	TODO
+    if (domain.toLowerCase() !== "lcp360") return;
+    else if (LCP360[data.fnc]) LCP360[data.fnc](data.param);
+	*/
+	
+	PANOSKIN[data.fnc](data.param);
+ 
+});
